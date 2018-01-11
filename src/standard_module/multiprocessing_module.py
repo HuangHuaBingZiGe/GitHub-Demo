053ae356a9b03f3c507ca8c3885e37d996569bc1
HuangHuaBingZiGe <yuezhen_wu@163.com>
2018-01-11 16:37:57 +0800

    20180108

--- src/standard_module/multiprocessing_module.py
+++ src/standard_module/multiprocessing_module.py
@@ -35,3 +35,79 @@ def f():
     l.x = 5
 threading.Thread(target=f).start()
 """
+
+"""
+from concurrent.futures import ThreadPoolExecutor
+executor = ThreadPoolExecutor(3)
+
+def f(a,b):
+    print('f',a,b)
+    return a ** b
+
+executor.submit(f,2,3)
+future = executor.submit(f,2,4)
+print(future.result())
+# 同时调用多个线程
+executor.map(f,[2,3,5],[4,5,6])
+
+import time
+
+def f(a,b):
+    print('f',a,b)
+    time.sleep(10)
+    return a ** b
+
+print(executor.map(f,[2,3,5,6,7],[4,5,6,7,8]))
+"""
+
+'''
+from multiprocessing import Process
+def f(s):
+    print(s)
+p = Process(target=f,args=('hello',))
+p.start()
+'''
+
+'''
+from multiprocessing import Process
+x = 1
+def f():
+    global x
+    x = 5
+f()
+print(x)
+x = 1
+p = Process(target=f)
+p.start()
+print(x)
+'''
+
+"""
+from multiprocessing import Queue,Pipe,Process
+q = Queue()
+q.put(1)
+print(q.get())
+def f(q):
+    print('start')
+    print(q.get())
+    print('end')
+Process(target=f,args=(q,)).start()
+print(q.put(100))
+"""
+
+"""
+from multiprocessing import Queue,Pipe,Process
+c1,c2 = Pipe()
+c1.send('abc')
+print(c2.recv())
+c2.send('xyz')
+print(c1.recv())
+
+def f(c):
+    c.send(c.recv() * 2)
+
+c1,c2 = Pipe()
+Process(target=f,args=(c2,)).start()
+c1.send(55)
+print(c1.recv())
+"""
--- src/using_tips/using_tips_5.py
+++ src/using_tips/using_tips_5.py
@@ -665,3 +665,569 @@ if __name__ == '__main__':
     httpd = ThreadingTCPServer(('',9000),Handler)
     httpd.serve_forever()
 """
+
+"""
+8-5 如何使用线程池
+
+实际案例：
+    我们之前实现了一个多线程web视频监控服务器，我们需要对请求连接数做限制。以防止恶意用户发起大量连接而导致服务器创建大量线程，最终因资源耗尽而瘫痪。
+    
+    可以使用线程池，替代原来的每次请求创建线程。
+    
+解决方案：
+    python3中有线程池实现
+    使用标准库中concurrent.futures下的ThreadPoolExector，对象的submit和map方法可以用来启动线程池中线程，执行任务
+
+"""
+
+"""
+import os, cv2, time, struct, threading
+from socketserver import TCPServer, ThreadingTCPServer
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from concurrent.futures import ThreadPoolExecutor
+from threading import Thread, RLock
+from select import select
+
+
+class JpegStreamer(Thread):
+    def __init__(self, camera):
+        Thread.__init__(self)
+        self.cap = cv2.VideoCapture(camera)
+        self.lock = RLock()
+        self.pipes = {}
+    
+    def register(self):
+        pr, pw = os.pipe()
+        self.lock.acquire()
+        self.pipes[pr] = pw
+        self.lock.release()
+        return pr
+    
+    def unregister(self, pr):
+        self.lock.acquire()
+        self.pipes.pop(pr)
+        self.lock.release()
+        pr.close()
+        pw.close()
+    
+    def capture(self):
+        cap = self.cap
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                ret, data = cv2.imencode('.jpg', frame, (cv2.IMWRITE_JPEG_QUALITY, 40))
+                yield data.toString()
+    
+    def send(self, frame):
+        n = struct.pack('l', len(frame))
+        self.lock.acquire()
+        if len(self.pipes):
+            _, pipes, _ = select([], self.pipes.itervalues(), [], 1)
+            for pipe in pipes:
+                os.write(pipe, n)
+                os.write(pipe, frame)
+        self.lock.release()
+    
+    def run(self):
+        for frame in self.capture():
+            self.send(frame)
+
+
+class JpegRetriever(object):
+    def __init__(self, streamer):
+        self.streamer = streamer
+        self.local = threading.local()
+    
+    def retriver(self):
+        while True:
+            ns = os.read(self.pipe, 8)
+            n = struct.unpack('l', ns)[0]
+            data = os.read(self.local.pipe, n)
+            yield data
+    
+    def __enter__(self):
+        if hasattr(self.local, 'pipe'):
+            raise RuntimeError()
+        
+        self.pipe = streamer.register()
+        return self.retriver()
+    
+    def __exit__(self, *args):
+        self.streamer.unregister(self.local.pipe)
+        del self.local.pipe
+        return True
+
+
+class Handler(BaseHTTPRequestHandler):
+    retriever = None
+    
+    @staticmethod
+    def setJpegRetriever(retriever):
+        Handler.retriever = retriever
+    
+    def do_Get(self):
+        if self.retriever is None:
+            raise RuntimeError('no retriver')
+        
+        if self.path != '/':
+            return
+        
+        self.send_response(200)
+        self.send_header("Content-type", 'multipart/x-mixed-replace;boundary=abcde')
+        self.end_headers()
+        
+        with self.retriever as frames:
+            for frame in frames:
+                self.send_frame(frame)
+    
+    def send_frame(self,frame):
+        s = '--abcde\r\n'
+        s += 'Content-Type:image/jpeg\r\n'
+        self.write.write(s.encode('ascii'))
+        self.write.write(frame)
+        
+class ThreadingPoolTCPServer(ThreadingTCPServer):
+    def __init__(self, server_address, RequestHandlerClass, bind_and_activate=True,max_thread_num=100):
+        super().__init__(server_address, RequestHandlerClass, bind_and_activate)
+        self.executor = ThreadPoolExecutor(max_thread_num)
+    
+    def process_request(self, request, client_address):
+        self.executor.submit(self.process_request_thread,request,client_address)
+if __name__ == '__main__':
+    streamer = JpegStreamer(0)
+    streamer.start()
+    
+    retriever = JpegRetriever(streamer)
+    Handler.setJpegRetriever(retriever)
+    
+    print('Start server...')
+    httpd = ThreadingPoolTCPServer(('', 9000), Handler,max_thread_num=3)
+    httpd.serve_forever()
+"""
+
+"""
+8-6 如何使用多进程
+
+实际案例：
+    由于python中全局解释器锁（GIL）的存在，在任意时刻只允许一个线程在解释器中运行，因此python的多线程不适合处理cpu密集型的任务
+    
+    想要处理cpu密集型的任务，可以使用多进程模型
+    
+解决方案：
+    使用标准库中的multiprocessing.Process，它可以启动子进程执行任务，操作接口，进程间通信，进程间同步等都与Threading.Thread类似
+
+"""
+
+"""
+from multiprocessing import Process
+from threading import Thread
+
+def isArmstrong(n):
+    a,t = [],n
+    while t > 0:
+        a.append(t % 10)
+        t /= 10
+    k = len(a)
+    return sum(x ** k for x in a) == n
+
+def findArmstrong(a,b):
+    print(a,b)
+    res = [k for k in range(a,b) if isArmstrong(k)]
+    print('%s - %s:%s' % (a,b,res))
+    
+def findByThread(*argslist):
+    workers = []
+    for args in argslist:
+        worker = Thread(target=findArmstrong,args=args)
+        workers.append(worker)
+        worker.start()
+
+    for worker in workers:
+        worker.join()
+        
+def findByProcess(*argslist):
+    workers = []
+    for args in argslist:
+        worker = Process(target=findArmstrong,args=args)
+        workers.append(worker)
+        worker.start()
+
+    for worker in workers:
+        worker.join()
+        
+if __name__ == '__main__':
+    import time
+    start = time.time()
+    # 多进程会更快
+    findByProcess((20000000,25000000),(25000000,30000000))
+    #findByThread((20000000,25000000),(25000000,30000000))
+    print(time.time() - start)
+"""
+
+"""
+9-1 如何使用函数装饰器
+
+实际案例：
+    某些时候我们想为多个函数，统一添加某种功能，比如计时统计，记录日志，缓存运算结果等等
+    
+    我们不想在每个函数内--添加完全相同的代码，有什么好的解决方案？
+    
+解决方案：
+    定义装饰器函数，用它来生成一个在原函数基础添加了新功能的函数，替代原函数
+    
+    
+题目1：斐波那契数列，又称黄金分割数列，指的是这样一个数列，1,1,2,3,5,8,13,21....，这个数列从第三项开始，每一项都等于前两项之和，求数列第n项
+
+def fibonacci(n,cache=None):
+    if cache is None:
+        cache == {}
+    if n in cache:
+        return cache[n]
+    if n <= 1:
+        return 1
+    cache[n] = fibonacci(n - 1,cache) + fibonacci(n - 2,cache)
+    return cache[n]
+    
+
+def memo(func):
+    cache = {}
+    def wrap(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return wrap
+
+@memo
+def fibonacci(n):
+    if n <= 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+# fibonacci = memo(fibonacci) 这条和 @memo 等价
+print(fibonacci(50))
+
+
+题目2：一个共有10个台阶的楼梯，从下面走到上面，一次只能迈1-3个台阶，并且不能后退，走完这个楼梯共有多少种方法
+
+@memo
+def climb(n,steps):
+    count = 0
+    if n == 0:
+        count = 1
+    elif n > 0:
+        for step in steps:
+            count += climb(n - step, steps)
+    return count
+
+print(climb(10,(1,2,3)))
+
+"""
+
+"""
+9-2 如何为被装饰的函数保存元数据
+
+实际案例：
+    在函数对象中保存着一些函数的元数据，例如：
+    f.__name__     : 函数的名字
+    f.__doc__      : 函数文档字符串
+    f.__module__   : 函数所属模块名
+    f.__dict__     : 属性字典
+    f.__defaults__ : 默认参数元组
+    
+我们在使用装饰器后，再使用上面这些属性访问时，看到的是内部包裹函数的元数据，原来函数的元数据便丢失掉了，应该如何解决？
+
+解决方案：
+    使用标准库functools中的装饰器wraps装饰内部包裹函数，可以制定将原函数的某些属性，更新到包裹函数上面
+
+def f(a):
+    '''f function'''
+    return a * 2
+print(f.__name__)
+g = f
+print(g.__name__)
+print(f.__doc__)
+print(f.__module__)
+print(f.__defaults__)
+def f(a,b=1,c=[]):
+    print(a,b,c)
+# defaults 保存默认参数
+print(f.__defaults__)
+f.__defaults__[1].append('abc')
+# 默认参数尽量不要用可变对象 []
+print(f(100))
+print(f.__closure__)
+
+def f():
+    a = 2
+    return lambda k: a ** k
+g = f()
+print(g.__closure__)
+c = g.__closure__[0]
+print(c.cell_contents)
+
+
+
+
+from functools import update_wrapper,wraps,WRAPPER_ASSIGNMENTS,WRAPPER_UPDATES
+
+def mydecorator(func):
+    @wraps(func)
+    def wrapper(*args,**kargs):
+        '''wrapper function'''
+        print('In wrapper')
+        func(*args, **kargs)
+    #update_wrapper(wrapper,func,('__name__','__doc__'),('__dict__',))
+    #update_wrapper(wrapper, func)
+    return wrapper()
+
+@mydecorator
+def example():
+    '''example function'''
+    print('In example')
+    
+print(example.__name__)
+print(example.__doc__)
+#print(WRAPPER_UPDATES)
+#print(WRAPPER_ASSIGNMENTS)
+
+"""
+
+"""
+9-3 如何定义带参数的装饰器
+
+实际案例：
+    实现一个装饰器，它用来检查被装饰函数的参数类型，装饰器可以通过参数指明函数参数的类型，调用时如果检测出类型不匹配则抛出异常
+    
+@typeassert(str,int,int)
+def f(a,b,c):
+    ...
+
+@typeassert(y=list)
+def g(x,y):
+    ...
+    
+解决方案：
+    提取函数签名：inspect.signature()
+    带参数的装饰器，也就是根据参数定制化一个装饰器，可以看成生产装饰器的工厂，每次调用typeassert，返回一个特定的装饰器，然后用它去修饰其他函数
+    
+from inspect import signature
+
+def typeassert(*ty_args,**ty_kargs):
+    def decorator(func):
+        # func -> a,b
+        # d = {'a':int,'b':str}
+        sig = signature(func)
+        # 部分参数绑定
+        btypes = sig.bind_partial(*ty_args,**ty_kargs).arguments
+        def wrapper(*args,**kargs):
+            # arg in d,instance(arg,d[arg])
+            for name,obj in sig.bind(*args, **kargs).arguments.items():
+                if name in btypes:
+                    if not isinstance(obj,btypes[name]):
+                        raise TypeError('"%s" must be "%s"' %(name,btypes[name]))
+            return func(*args,**kargs)
+        return wrapper
+    return decorator
+
+@typeassert(int,str,list)
+def f(a,b,c):
+    print(a,b,c)
+f(1,'abc',[1,2,3])
+f(1,2,[1,2,3])
+
+
+from inspect import signature
+def f(a,b,c=1):
+    pass
+sig = signature(f)
+print(sig.parameters)
+a = sig.parameters['a']
+print(a.name)
+print(a.kind)
+print(a.default)
+c = sig.parameters['c']
+print(c.default)
+print(sig.bind(str,int,int))
+bargs = sig.bind(str,int,int)
+print(bargs.arguments)
+print(bargs.arguments['a'])
+print(bargs.arguments['b'])
+#print(sig.bind(str)) 做类型检查，参数必须填
+print(sig.bind_partial(str))
+# 只对部分参数校验
+
+"""
+
+"""
+9-4 如何实现属性可修改的函数装饰器
+
+实际案例：
+    为分析程序内哪些函数执行时间开销较大，我们定义一个带timeout参数的函数装饰器，装饰功能如下：
+    
+    1.统计被装饰函数单次调用运行时间
+    2.时间大于参数timeout的，将此次函数调用记录到log日志中
+    3.运行时可修改timeout的值
+
+解决方案：
+    为包裹函数增添一个函数，用来修改闭包中使用的自由变量，在python3中：使用non访问嵌套作用域中的变量引用
+
+"""
+
+"""
+#python2
+from functools import wraps
+import time
+import logging
+
+def warn(timeout):
+    timeout = [timeout]
+    def decorator(func):
+        def wrapper(*args,**kargs):
+            start = time.time()
+            res = func(*args,**kargs)
+            used = time.time() - start
+            if used > timeout:
+                msg = '"%s": %s > %s' %(func.__name__,used,timeout[0])
+                logging.warn(msg)
+            return res
+        def setTimeout(k):
+            # nonlocal timeout
+            timeout[0] = k
+        wrapper.setTimeout = setTimeout
+        return wrapper
+    return decorator
+
+from random import randint
+@warn(1.5)
+def test():
+    print('In test')
+    while randint(0,1):
+        time.sleep(0.5)
+
+for _ in range(30):
+    test()
+    
+test.setTimeout(1)
+for _ in range(30):
+    test()
+"""
+
+"""
+#python3
+from functools import wraps
+import time
+import logging
+
+def warn(timeout):
+    def decorator(func):
+        def wrapper(*args, **kargs):
+            start = time.time()
+            res = func(*args, **kargs)
+            used = time.time() - start
+            if used > timeout:
+                msg = '"%s": %s > %s' % (func.__name__, used, timeout)
+                logging.warn(msg)
+            return res
+        def setTimeout(k):
+            nonlocal timeout
+            timeout = k
+        wrapper.setTimeout = setTimeout
+        return wrapper
+    return decorator
+
+from random import randint
+@warn(1.5)
+def test():
+    print('In test')
+    while randint(0, 1):
+        time.sleep(0.5)
+
+for _ in range(30):
+    test()
+
+test.setTimeout(1)
+for _ in range(30):
+    test()
+"""
+
+"""
+9-5 如何在类中定义装饰器
+
+实际案例：
+    实现一个能将函数调用信息记录到日志的装饰器：
+    1.把每次函数的调用时间，执行时间，调用次数写入日志
+    2.可以对被装饰器函数分组，调用信息记录到不同日志
+    3.动态修改参数，比如日志格式
+    4.动态打开关闭日志输出功能
+    
+解决方案：
+    为了让装饰器在使用上更为灵活，可以把类的实例方法作为装饰器，此时在包裹函数中就可以持有实例对象，便于修改属性和拓展功能
+
+"""
+
+"""
+import logging
+import time
+from time import localtime,time,strftime,sleep
+
+class CallingInfo(object):
+    def __init__(self,name):
+        log = logging.getLogger(name)
+        log.setLevel(logging.INFO)
+        fh = logging.FileHandler(name + '.log')
+        log.addHandler(fh)
+        log.info('Start'.center(50,'-'))
+        self.log = log
+        self.formatter = '%(func)s -> [%(time)s - %(used)s - %(ncalls)s]'
+        
+    def info(self,func):
+        def wrapper(*args,**kargs):
+            wrapper.ncalls += 1
+            lt = localtime()
+            start = time()
+            res = func(*args,**kargs)
+            used = time() - start
+            info = {}
+            info['func'] = func.__name__
+            info['time'] = strftime('%x %X',lt)
+            info['used'] = used
+            info['ncalls'] = wrapper.ncalls
+            msg = self.formatter % info
+            self.log.info(msg)
+            return res
+        wrapper.ncalls = 0
+        return wrapper
+    
+    def setFormatter(self,formatter):
+        self.formatter = formatter
+        
+    def turnOn(self):
+        self.log.setLevel(logging.INFO)
+        
+    def turnOff(self):
+        self.log.setLevel(logging.WARN)
+
+cinfo1 = CallingInfo('mylog1')
+cinfo2 = CallingInfo('mylog2')
+
+cinfo1.setFormatter('%(func)s -> [%(time)s - %(ncalls)s]')
+cinfo2.turnOff()
+
+@cinfo1.info
+def f():
+    print('in f')
+    
+@cinfo1.info
+def g():
+    print('in g')
+
+@cinfo2.info
+def h():
+    print('in h')
+
+from random import choice
+
+for _ in range(50):
+    choice([f,g,h])()
+    sleep(choice([0.5,1,2]))
+"""
